5128dea928344002d2b8c8e75634889297fadb96
soumaysoni <soumaysoni1687@gmail.com>
2026-02-07 18:00:49 +0530

    Initial React + TypeScript project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center">DSV React CRUD Assignment</h2>
+    </div>
+  );
+}
+
+export default App;
+
