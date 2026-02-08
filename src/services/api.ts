94c20beeb2ea12e5977131476e33357cdacf5515
soumaysoni <soumaysoni1687@gmail.com>
2026-02-08 15:57:49 +0530

    Update API base URL to production and add start script for json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://dsv-react-assignment.onrender.com",
 });
 
 export const userApi = {
